51985d57c1294c1bbe2cb265fd37b6559035eb54
bradchao <brad@brad.tw>
2020-09-19 13:29:45 +0800

    3

--- app/src/main/java/tw/org/tcca/apps/test3/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/test3/MainActivity.java
@@ -3,10 +3,15 @@ package tw.org.tcca.apps.test3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,5 +28,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test1(View view) {
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                "http://www.tcca.org.tw",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.v("bradlog", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.v("bradlog", error.toString());
+                    }
+                }
+        );
+        queue.add(request);
     }
 }
