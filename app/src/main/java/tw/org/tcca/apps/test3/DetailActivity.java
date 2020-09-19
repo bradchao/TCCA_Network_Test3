602de61b9061c79391bbc60f4fe266265aedb7dc
bradchao <brad@brad.tw>
2020-09-19 15:26:54 +0800

    13

--- app/src/main/java/tw/org/tcca/apps/test3/DetailActivity.java
+++ app/src/main/java/tw/org/tcca/apps/test3/DetailActivity.java
@@ -2,9 +2,13 @@ package tw.org.tcca.apps.test3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
 
 public class DetailActivity extends AppCompatActivity {
     private TextView salePlace, feature;
@@ -21,6 +25,23 @@ public class DetailActivity extends AppCompatActivity {
 
         salePlace.setText(MainApp.detailData.get("SalePlace"));
         feature.setText(MainApp.detailData.get("Feature"));
-
+        fetchImage();
     }
+
+    private void fetchImage(){
+        ImageRequest request = new ImageRequest(
+                MainApp.detailData.get("Column1"),
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        img.setImageBitmap(response);
+                    }
+                },
+                0,0,
+                Bitmap.Config.ARGB_8888,
+                null
+        );
+        MainApp.queue.add(request);
+    }
+
 }
