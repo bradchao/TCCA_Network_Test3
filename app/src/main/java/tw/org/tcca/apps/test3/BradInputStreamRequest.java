9a78ae4232bdb7f9324ac0aba38801901c20f242
bradchao <brad@brad.tw>
2020-09-19 15:43:46 +0800

    14

---
+++ app/src/main/java/tw/org/tcca/apps/test3/BradInputStreamRequest.java
@@ -0,0 +1,41 @@
+package tw.org.tcca.apps.test3;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import java.util.Map;
+
+public class BradInputStreamRequest extends Request<byte[]> {
+    private Response.Listener<byte[]> listener;
+    private Map<String,String> responseHeader;
+    private Map<String,String> params;
+
+    public BradInputStreamRequest(int method, String url,
+                                  Map<String,String> params,
+                                  Response.Listener<byte[]> listener,
+                                  @Nullable Response.ErrorListener errlistener) {
+        super(method, url, errlistener);
+        this.listener = listener;
+        this.params = params;
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    @Override
+    protected Response<byte[]> parseNetworkResponse(NetworkResponse response) {
+        responseHeader = response.headers;
+        return Response.success(response.data, HttpHeaderParser.parseCacheHeaders(response));
+    }
+
+    @Override
+    protected void deliverResponse(byte[] response) {
+        listener.onResponse(response);
+    }
+}
