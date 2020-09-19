914f6a94af5fbd708afe7f0c1d1333035e54772d
bradchao <brad@brad.tw>
2020-09-19 13:21:39 +0800

    2

--- app/src/main/java/tw/org/tcca/apps/test3/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/test3/MainActivity.java
@@ -3,12 +3,25 @@ package tw.org.tcca.apps.test3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+        queue = Volley.newRequestQueue(this);
+    }
+
+    public void test1(View view) {
     }
 }
